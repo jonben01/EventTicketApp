f310a870e54c49ede068c2df2e383f21a0e39df3
Peteris Meiris <petmei01@easv365.dk>
2025-02-13 17:07:47 +0100

    Event Dashboard

--- src/main/java/dk/easv/ticketapptest/GUI/Controllers/EventDashboardController.java
+++ src/main/java/dk/easv/ticketapptest/GUI/Controllers/EventDashboardController.java
@@ -1,4 +1,19 @@
 package dk.easv.ticketapptest.GUI.Controllers;
 
+import dk.easv.ticketapptest.BE.Event;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class EventDashboardController {
+    @FXML
+    private VBox sidebarVBox;
+    @FXML
+    private HBox topbarHBox;
+
+    @FXML
+    public void initialize() {
+        sidebarVBox.getStyleClass().add("sidebar");
+        topbarHBox.getStyleClass().add("topbar");
+    }
 }
