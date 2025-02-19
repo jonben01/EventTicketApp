95720ccfd0df985ad2b3f919e6f840fd3682897f
jonben01 <jonasbendorff@gmail.com>
2025-02-19 16:57:44 +0100

    styling in admin dashboard + create user view

--- src/main/java/dk/easv/ticketapptest/GUI/Controllers/AdminDashboardController.java
+++ src/main/java/dk/easv/ticketapptest/GUI/Controllers/AdminDashboardController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -13,21 +14,30 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AdminDashboardController {
-    public BorderPane rootPaneAdmin;
+    @FXML public BorderPane rootPaneAdmin;
+    @FXML public Button btnUserManagement;
+    @FXML public Button btnEventHistory;
 
 
     public void handleUserManagement(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/user-management-view.fxml"));
         Parent adminDashboard = fxmlLoader.load();
 
+        btnUserManagement.setStyle("-fx-background-color: #EFF6FF; -fx-text-fill: #1D4ED8;");
+        btnEventHistory.setStyle("");
+
         rootPaneAdmin.setCenter(adminDashboard);
 
 
     }
 
+
     public void handleEventHistory(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/admin-event-view.fxml"));
         Parent adminDashboard = fxmlLoader.load();
+
+        btnEventHistory.setStyle("-fx-background-color: #EFF6FF; -fx-text-fill: #1D4ED8;");
+        btnUserManagement.setStyle("");
 
         rootPaneAdmin.setCenter(adminDashboard);
     }
