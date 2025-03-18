00c5bf727a3650144b1059a234706c90a540a676
David Jonas <davjon01@easv365.dk>
2025-03-18 16:13:48 +0100

    Ticket Crud
    Finished Crud for deleteTicket, updateTicket, getAllTickets, getTicketsForEvent and AssignTicketToEvent

--- src/main/java/dk/easv/ticketapptest/DAL/ITicketDataAccess.java
+++ src/main/java/dk/easv/ticketapptest/DAL/ITicketDataAccess.java
@@ -10,14 +10,14 @@ public interface ITicketDataAccess {
 
     public Ticket createTicket(Ticket ticket) throws SQLException;
 
-    public void deleteTicket(Ticket ticket);
+    public void deleteTicket(Ticket ticket) throws SQLException;
 
-    public Ticket updateTicket(Ticket ticket);
+    public Ticket updateTicket(Ticket ticket) throws SQLException;
 
-    public List<Ticket> getAllTickets();
+    public List<Ticket> getAllTickets() throws SQLException;
 
-    public List<Ticket> getTicketsForEvent(Event2 event);
+    public List<Ticket> getTicketsForEvent(Event2 event) throws SQLException;
 
-    public void AssignTicketToEvent(Ticket ticket, Event2 event);
+    public void AssignTicketToEvent(Ticket ticket, Event2 event) throws SQLException;
 
 }
--- src/main/java/dk/easv/ticketapptest/DAL/TicketDAO.java
+++ src/main/java/dk/easv/ticketapptest/DAL/TicketDAO.java
@@ -5,6 +5,7 @@ import dk.easv.ticketapptest.BE.Ticket;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TicketDAO implements ITicketDataAccess {
@@ -49,27 +50,94 @@ public class TicketDAO implements ITicketDataAccess {
     }
 
     @Override
-    public void deleteTicket(Ticket ticket) {
+    public void deleteTicket(Ticket ticket) throws SQLException {
+        String sql = "DELETE FROM dbo.Tickets WHERE TicketID = ?;";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, ticket.getTicketID());
+            ps.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new SQLException("Could not delete ticket", e);
+        }
+    }
+
+    @Override
+    public Ticket updateTicket(Ticket ticket) throws SQLException {
+        String sql = "UPDATE dbo.Tickets SET Title = ?, Description = ?, Price = ?, Global = ? WHERE TicketID = ?;";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, ticket.getTicketName());
+            ps.setString(2, ticket.getDescription());
+            ps.setFloat(3, (float) ticket.getPrice());
+            ps.setBoolean(4, ticket.isGLOBAL());
+            ps.setInt(5, ticket.getTicketID());
+            ps.executeUpdate();
+            return ticket;
+        } catch (SQLException e) {
+            throw new SQLException("Could not update ticket", e);
+        }
+
 
     }
 
     @Override
-    public Ticket updateTicket(Ticket ticket) {
-        return null;
+    public List<Ticket> getAllTickets() throws SQLException {
+        List<Ticket> allTickets = new ArrayList<>();
+        String sql = "SELECT * FROM dbo.Tickets;";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+             ResultSet rs = ps.executeQuery(sql);
+             while (rs.next()) {
+                 int ticketID = rs.getInt("TicketID");
+                 String title = rs.getString("Title");
+                 String description = rs.getString("Description");
+                 double price = rs.getDouble("Price");
+                 boolean GLOBAL = rs.getBoolean("Global");
+                 Ticket ticket = new Ticket(ticketID, price, GLOBAL, title, description);
+                 allTickets.add(ticket);
+             }
+            }catch (SQLException e) {
+            throw new SQLException("Could not get all tickets", e);
+        }
+        return allTickets;
     }
 
     @Override
-    public List<Ticket> getAllTickets() {
-        return List.of();
+    public List<Ticket> getTicketsForEvent(Event2 event) throws SQLException {
+        List<Ticket> eventtickets = new ArrayList<>();
+        String sql = "SELECT t.* FROM dbo.Tickets t JOIN dbo.TicketEvent_Junction te ON t.TicketID = te.TicketID WHERE te.EventID = ?;";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, event.getEventID());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int ticketID = rs.getInt("TicketID");
+                String title = rs.getString("Title");
+                String description = rs.getString("Description");
+                double price = rs.getDouble("Price");
+                boolean GLOBAL = rs.getBoolean("Global");
+                Ticket ticket = new Ticket(ticketID, price, GLOBAL, title, description);
+                eventtickets.add(ticket);
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Could not get tickets for event" , e);
+        }
+        return eventtickets;
     }
 
     @Override
-    public List<Ticket> getTicketsForEvent(Event2 event) {
-        return List.of();
-    }
+    public void AssignTicketToEvent(Ticket ticket, Event2 event) throws SQLException{
+        String sql = "INSERT INTO dbo.TicketEvent_Junction(TicketID, EventID) " + "VALUES(?, ?);";
+        try (Connection connection = connector.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, ticket.getTicketID());
+            ps.setInt(2, event.getEventID());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("Could not assign ticket to event", e);
+        }
 
-    @Override
-    public void AssignTicketToEvent(Ticket ticket, Event2 event) {
 
     }
 }
